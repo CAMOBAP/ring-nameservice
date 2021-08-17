f6c246c015956d155af53329d73e54c83008a93b
Alexande B <abobrikovich@gmail.com>
2021-08-17 08:48:14 +0200

    Fix unlockAccount call to make sure that unlock happens

--- deploy_contract.js
+++ deploy_contract.js
@@ -8,7 +8,6 @@ const passwd = fs.readFileSync("./data/passwd", { encoding : "utf-8" }).trim();
 
 const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-web3.eth.personal.unlockAccount(sender, passwd, 10);
 
 function contract(web3) {
   const json = JSON.parse(fs.readFileSync("./contract/registrar.out.json", { encoding : "utf-8" }));
@@ -18,10 +17,10 @@ function contract(web3) {
 }
 
 async function main() {
+  await web3.eth.personal.unlockAccount(sender, passwd, 10 /* secs */)
   const registrarContract = contract(web3);
   const instance = await registrarContract.deploy().send({ from: sender, gas: 1000000 });
-  const address = instance.options.address;
-  console.log(address);
+  console.log(instance.options.address);
 }
 
 main();
